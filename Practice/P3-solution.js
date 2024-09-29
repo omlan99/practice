a6dee6bbe4f9b741333b53f58dfd7b1762626a55
omlan99 <omlan10616@gmail.com>
2024-09-29 20:43:39 +0600

    reduce method

--- Practice/P3-solution.js
+++ Practice/P3-solution.js
@@ -3,7 +3,6 @@
 //  solution using for loop 
 const squareSumAvg = arr => {
     const square = arr.map(num => num*num);
-    // const sumOfSquare = square.reduce((arg1, arg2) => arg1 + arg2)
     let sum = 0
     for(const num of square){
          sum = sum + num
@@ -17,3 +16,13 @@ const squareSumAvg = arr => {
 
  
 //    solution using reduce method
+const squareSumAvg2 = arr =>{
+          // mapping the array to get array element one by one
+         const square = arr.map(num => num*num);
+          // reduce take two arguement and add them
+         const sumOfSquare = square.reduce((arg1, arg2) => arg1 + arg2)
+          // average calculation
+         const average = sumOfSquare / arr.length;
+         return average;
+}
+   console.log(squareSumAvg2(numbers))
